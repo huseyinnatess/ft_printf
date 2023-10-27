d624ba54bea6317857a6923b01f077e2e7317996
Hüseyin Ateş <109745789+huseyinnatess@users.noreply.github.com>
2023-10-27 16:15:59 +0300

    Add files via upload

--- ft_itoa.c
+++ ft_itoa.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
 #include <stdlib.h>
 
 static int	ft_count(long int num)
