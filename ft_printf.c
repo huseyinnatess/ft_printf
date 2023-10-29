67aed17bde5239c9eb0b0c8453567d7ef3b6944b
Hüseyin Ateş <109745789+huseyinnatess@users.noreply.github.com>
2023-10-29 11:13:45 +0300

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: huates <huates@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 16:01:10 by huates            #+#    #+#             */
-/*   Updated: 2023/10/25 16:31:54 by huates           ###   ########.fr       */
+/*   Updated: 2023/10/29 11:13:02 by huates           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static int	ft_putchar(char c)
 	return (write(1, &c, 1));
 }
 
-static int	ft_format(va_list arg, char flag)
+static int	ft_format(va_list arg, char flag) // Gelen flag'e göre (c, d, i, ...) format fonksiyonunu çağırır.
 {
 	if (flag == 'c')
 		return (ft_putchar(va_arg(arg, int)));
@@ -33,17 +33,17 @@ static int	ft_format(va_list arg, char flag)
 		return (ft_print_unsigned(va_arg(arg, unsigned int)));
 	else if (flag == 'x' || flag == 'X')
 		return ((ft_hexadecimal(va_arg(arg, unsigned int), flag)));
-	if (flag == '%')
+	if (flag == '%') // % işareti basar.
 	{
 		if (ft_putchar('%') < 0)
 			return (-1);
 		return (1);
 	}
-	return (-1);
+	return (-1); // Fonksiyon int olduğu için herhangi bir değer return etmek zorunda. Fonksiyon çalışmasına etki etmez.
 }
 
-static int	ft_flag_check(const char *str, int i)
-{
+static int	ft_flag_check(const char *str, int i) // Gelen stringin i. indexindeki karakter % ise ve bir sonraki karakter format karakteri ise 1 return eder.
+{												  // Aksi halde 0 return eder.
 	return (str[i] == '%' && (str[i + 1] == 'c' || str[i + 1] == 'd' || str[i
 				+ 1] == 'i' || str[i + 1] == 'u' || str[i + 1] == 'x' || str[i
 				+ 1] == 'X' || str[i + 1] == 'p' || str[i + 1] == 's' || str[i
@@ -52,20 +52,20 @@ static int	ft_flag_check(const char *str, int i)
 
 static int	ft_check_str(const char *str, int *rtn, va_list arg)
 {
-	int	format;
+	int	format; // write fonksiyonunun dönüş değerini tutar.
 	int	i;
 
 	i = -1;
 	while (str[++i])
 	{
-		if (ft_flag_check(str, i))
+		if (ft_flag_check(str, i)) // Bu if bloğu sadece doğru bir format karakteri geldiğinde çalışır.
 		{
 			format = ft_format(arg, str[++i]);
 			if (format == -1)
 				return (-1);
 			*rtn += format;
 		}
-		else
+		else // Bu else bloğu format karakteri yanlış geldiğinde, sadece % işareti geldiğinde veya string bir karakter geldiğinde çalışır.
 		{
 			if (str[i] == '%')
 				return (0);
@@ -76,17 +76,20 @@ static int	ft_check_str(const char *str, int *rtn, va_list arg)
 		}
 	}
 	return (0);
+
+	// Fonksiyonların dönüş değerlerini format değişkenine atamak zorundayız çünkü direk rtn değerine ekler isek 
+	// hatalı sonuçlar alırız. Sebebi için readme.
 }
 
-int	ft_printf(const char *str, ...)
+int	ft_printf(const char *str, ...) // Variable arguments fonksiyonu. Sınırsız sayıda argüman alabilir.
 {
-	va_list	arg;
+	va_list	arg; // Argümanları tutar.
 	int		rtn;
 
 	rtn = 0;
-	va_start(arg, str);
+	va_start(arg, str); // Argümanları almayı başlatır. ilk argüman olarak argümanları tutan değişkeni, ikinci argüman olarak ise hangi veriden sonra argümanları alacağını belirler.
 	if (ft_check_str(str, &rtn, arg) < 0)
 		return (-1);
-	va_end(arg);
-	return (rtn);
+	va_end(arg); // Argümanları almayı bitirir.
+	return (rtn); // Yazdırılan karakter sayısını return eder.
 }
--- ft_printf_utlis.c
+++ ft_printf_utlis.c
@@ -6,7 +6,7 @@
 /*   By: huates <huates@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 16:15:40 by huates            #+#    #+#             */
-/*   Updated: 2023/10/24 14:37:37 by huates           ###   ########.fr       */
+/*   Updated: 2023/10/29 10:55:31 by huates           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_putstr(char *str)
 	int	i;
 
 	i = -1;
-	if (!str)
+	if (!str) // Eğer gelen string NULL ise "(null)" basar.
 		return (write(1, "(null)", 6));
 	while (str[++i])
 	{
@@ -29,18 +29,18 @@ int	ft_putstr(char *str)
 	return (i);
 }
 
-int	ft_hexadecimal(unsigned int number, char c)
+int	ft_hexadecimal(unsigned int number, char c) // Gelen sayıyı hexadecimal olarak basar. Recursive olarak çalışır.
 {
-	int	rtn;
-	int	tmp;
+	int	rtn; // return değerini tutar.
+	int	tmp; // write fonksiyonunun dönüş değerini tutar.
 
 	rtn = 0;
 	if (number >= 16)
 	{
-		tmp = ft_hexadecimal(number / 16, c);
-		if (tmp < 0)
+		tmp = ft_hexadecimal(number / 16, c); 
+		if (tmp < 0) // Eğer write fonksiyonu başarısız olursa -1 döndürür.
 			return (-1);
-		rtn += tmp;
+		rtn += tmp; // return değerine yazdırılan karakter sayısını ekler.
 	}
 	if (c == 'X')
 		tmp = write(1, &"0123456789ABCDEF"[number % 16], 1);
@@ -48,23 +48,23 @@ int	ft_hexadecimal(unsigned int number, char c)
 		tmp = write(1, &"0123456789abcdef"[number % 16], 1);
 	if (tmp < 0)
 		return (-1);
-	return (rtn + 1);
+	return (rtn + 1); // return değerine yazdırılan karakter sayısını ekler. Her seferinde mantıken 1 karakter yazdırıldığı için 1 eklenir.
 }
 
-int	ft_pointer(unsigned long int number, int check)
+int	ft_pointer(unsigned long int number, int check) // Gelen sayının adresini hexadecimal olarak basar. Recursive olarak çalışır.
 {
 	int	rtn;
 	int	tmp;
 
 	rtn = 0;
-	if (check)
-		if ((write(1, "0x", 2)) < 0)
+	if (check) // Recursive olarak çalıştığı için tekrar tekrar 0x yazdırmamak için kontrol eder.
+		if ((write(1, "0x", 2)) < 0) // Adres formatı 0x ile başlar. Eğer write fonksiyonu başarısız olursa -1 döndürür.
 			return (-1);
 	if (check)
-		rtn += 2;
+		rtn += 2; // "0x" yazdırıldığı için return değerine 2 ekler.
 	if (number >= 16)
 	{
-		tmp = ft_pointer(number / 16, 0);
+		tmp = ft_pointer(number / 16, 0); // Recursive olarak çalıştığı için check değeri 0 olarak gönderilir. "0x" tekrar tekrar yazdırılmaması için.
 		if (tmp < 0)
 			return (-1);
 		rtn += tmp;
@@ -75,16 +75,16 @@ int	ft_pointer(unsigned long int number, int check)
 	return (rtn + 1);
 }
 
-int	ft_print_number(long int number)
+int	ft_print_number(long int number) // Gelen sayıyı basar.
 {
-	char	*str;
+	char	*str; // Sayıyı string olarak tutar.
 	int		rtn;
 
-	str = ft_itoa(number);
-	if (!str)
+	str = ft_itoa(number); // Sayıyı string olarak çevirir.
+	if (!str) // Eğer malloc fonksiyonu başarısız olursa NULL döndürür.
 		return (-1);
 	rtn = ft_putstr(str);
-	free(str);
+	free(str); // Malloc fonksiyonu ile ayrılan alanı free fonksiyonu ile geri verir.
 	return (rtn);
 }
 
